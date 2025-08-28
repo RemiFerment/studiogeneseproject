52751686bceb4429f54c820d40561bcf894f39d1
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-08-28 21:01:41 +0200

    adding form feature

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,12 +10,16 @@ const Contact = () => {
         </div>
         <div className="col-lg-9 text-center d-flex flex-column justify-content-center align-items-center">
           <h1 className='my-5 text-uppercase fw-bold'>Formulaire de contact</h1>
-          <div className="d-flex ">
-            <h1>
-              <i class="bi bi-cone-striped text-warning"></i>
-              Cette partie du site n'est pas encore fini...
-              <i class="bi bi-cone-striped text-warning"></i>
-            </h1>
+          <div className="w-75 text-start">
+            <form name="contact" method="post" className='d-flex flex-column gap-2' data-netlify="true" data-netlify-honeypot="bot-field">
+              <label className='mt-3'>Votre nom: </label>
+              <input type="text" name="name" />
+              <label className='mt-3'>Email: </label>
+              <input type="email" name="email" />
+              <label className='mt-3'>Message:</label>
+              <textarea name="message" rows="8"></textarea>
+              <button type="submit" className='btn btn-success sg-bg w-25'>Envoyer</button>
+            </form>
           </div>
         </div>
       </div>
